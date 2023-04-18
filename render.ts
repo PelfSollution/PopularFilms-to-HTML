af56f0bf79449369d02308f14c7f8752c0052970
David A <daayros@protonmail.com>
2023-04-18 11:17:06 +0200

    pruebas de mejoras de typado + plantilla en render.ts + modificaciones CSS

--- render.ts
+++ render.ts
@@ -1,25 +1,36 @@
 import { Film } from "./films.ts";
 
+function createParagraph(title: string, content: string): string {
+  return `
+    <p>
+      <strong>${title}:</strong> ${content}
+    </p>
+  `;
+}
+
 const renderFilmCard = (film: Film): string => {
+  const fechaLanzamiento = createParagraph("Fecha de lanzamiento", film.release_date);
+  const director = createParagraph("Director", film.director);
   return `
     <div class="film-card">
       <img src="${film.getImageUrl()}" alt="${film.title}" />
       <h2>${film.title}</h2>
-      <p><strong>Fecha de lanzamiento:</strong> ${film.release_date}</p>
-      <p><strong>Director:</strong> ${film.director}</p>
+      ${fechaLanzamiento}
+      ${director}
       <a href="films/film-${film.id}.html">Detalles</a>
     </div>
   `;
 };
 
 const renderFilmDetails = (film: Film): string => {
+  const fechaLanzamiento = createParagraph("Fecha de lanzamiento", film.release_date);
+  const director = createParagraph("Director", film.director);
   return `
     <div class="film-card-interior">
       <img src="${film.getImageUrl()}" alt="${film.title}">
       <h2>${film.title}</h2>
-      <p><strong>Fecha de lanzamiento:</strong> ${film.release_date}</p>
-      <p><strong>Director:</strong> ${film.director}</p>
-      <p><strong>Sinopsis:</strong></p>
+      ${fechaLanzamiento}
+      ${director}
       <p>${film.overview}</p>
       <a href="../index.html" class="volver">Volver al listado</a>
     </div>
@@ -66,7 +77,7 @@ export function renderDetails(film: Film): string {
       </head>
       <body>
         <header>
-          <h1>${film.title}</h1>
+          <h1><img src="../img/logo.svg" alt="Logo tmdb">${film.title}</h1>
         </header>
         <main>
           <div class="film-container-details">
@@ -77,3 +88,4 @@ export function renderDetails(film: Film): string {
     </html>
   `;
 };
+
